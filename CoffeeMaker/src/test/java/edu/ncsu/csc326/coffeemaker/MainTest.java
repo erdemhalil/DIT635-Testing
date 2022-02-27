f2140a1d6ae25353858a0fa8c325fa2511b34888
Erdem Halil <71549844+erdemhalil@users.noreply.github.com>
2022-02-27 15:56:11 +0100

    Add tests

--- CoffeeMaker/src/test/java/edu/ncsu/csc326/coffeemaker/MainTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc326/coffeemaker/MainTest.java
@@ -1,31 +1,30 @@
-package test.java.edu.ncsu.csc326;
+package edu.ncsu.csc326.coffeemaker;
 
-import edu.ncsu.csc326.coffeemaker.CoffeeMaker;
-import edu.ncsu.csc326.coffeemaker.Recipe;
 import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
-
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.function.Try;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
-
+	
 	private CoffeeMaker cm;
 	private Recipe r1;
 	private Recipe r2;
 	private Recipe r3;
 	private Recipe r4;
 
-	@BeforeAll
+	@Before
 	public void setUp() throws Exception {
 		cm = new CoffeeMaker();
-
-		// Set up for r1
+		
+		//Set up for r1
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setAmtChocolate("0");
@@ -33,9 +32,8 @@ public class MainTest {
 		r1.setAmtMilk("1");
 		r1.setAmtSugar("1");
 		r1.setPrice("50");
-		cm.addRecipe(r1);
-
-		// Set up for r2
+		
+		//Set up for r2
 		r2 = new Recipe();
 		r2.setName("Mocha");
 		r2.setAmtChocolate("20");
@@ -43,9 +41,8 @@ public class MainTest {
 		r2.setAmtMilk("1");
 		r2.setAmtSugar("1");
 		r2.setPrice("75");
-		cm.addRecipe(r2);
-
-		// Set up for r3
+		
+		//Set up for r3
 		r3 = new Recipe();
 		r3.setName("Latte");
 		r3.setAmtChocolate("0");
@@ -53,9 +50,8 @@ public class MainTest {
 		r3.setAmtMilk("3");
 		r3.setAmtSugar("1");
 		r3.setPrice("100");
-		cm.addRecipe(r3);
-
-		// Set up for r4
+		
+		//Set up for r4
 		r4 = new Recipe();
 		r4.setName("Hot Chocolate");
 		r4.setAmtChocolate("4");
@@ -63,10 +59,38 @@ public class MainTest {
 		r4.setAmtMilk("1");
 		r4.setAmtSugar("1");
 		r4.setPrice("65");
-		cm.addRecipe(r4);
+	}
+	//0
+	@Test
+	public void testAddInventory_Normal() {
+		try {
+			cm.addInventory("4","7","0","9"); //Coffee, Milk, Sugar, Chocolate
+		} catch (InventoryException e) {
+			fail("InventoryException should not be thrown");
+		}
+		String inventory = cm.checkInventory();
+		String expected = "Coffee: 19\nMilk: 22\nSugar: 15\nChocolate: 24\n";
+		// We start with 15 in inventory, then added some.
+		assertEquals(expected,inventory);
+	}
+	//1
+	@Test
+	public void testAddInventoryException() {
+		Throwable exception = assertThrows(
+				InventoryException.class, () -> {
+					cm.addInventory("4", "-1", "asdf", "3"); // Should throw an InventoryException
+				}
+				);
+	}
+	
+	//2
+	@Test
+	public void testMakeCoffee_Normal() {
+		cm.addRecipe(r1);
+		assertEquals(25, cm.makeCoffee(0, 75));
 	}
 
-	//1
+	//3
 	@Test
 	public void testCheckInventory() {
 		String inventory = cm.checkInventory();
@@ -75,41 +99,20 @@ public class MainTest {
 		assertEquals(expected, inventory);
 	}
 
-	//2
-	@Test
-	public void testMakeCoffee() {
-		assertEquals(20, cm.makeCoffee(2, 120)); // Make a coffee and check the change
-		String inventory = cm.checkInventory();
-		String expected = "Coffee: 12\nMilk: 12\nSugar: 14\nChocolate: 15\n";
-		assertEquals(expected, inventory); // Check if the remaining ingrediants are correct
-	}
-
-	//3
-	@Test
-	public void testAddInventory_Normal() {
-		try {
-			cm.addInventory("4", "7", "0", "9"); // Coffee, Milk, Sugar, Chocolate
-		} catch (InventoryException e) {
-			fail("InventoryException should not be thrown");
-		}
-		String inventory = cm.checkInventory();
-		String expected = "Coffee: 16\nMilk: 19\nSugar: 14\nChocolate: 24\n";
-		// We add some inventory
-		assertEquals(expected, inventory);
-	}
-
 	//4
 	@Test
-	public void testAddInventoryException() {
-		Throwable exception = assertThrows(
-				InventoryException.class, () -> {
-					cm.addInventory("4", "-1", "asdf", "3"); // Should throw an InventoryException
-				});
+	public void testMakeCoffee() {
+		cm.addRecipe(r1);
+		int result = cm.makeCoffee(0, 120);
+		assertEquals(70, result); // Make a coffee and check the change
+		String inventory = cm.checkInventory();
+		String expected = "Coffee: 12\nMilk: 14\nSugar: 14\nChocolate: 15\n";
+		assertEquals(expected, inventory); // Check if the remaining ingrediants are correct
 	}
 
 	//5
 	@Test
-	public void testAddRecepi() {
+	public void testAddRecipe() {
 		Recipe r5 = new Recipe();
 		try {
 			r5.setName("Milk");
@@ -118,18 +121,18 @@ public class MainTest {
 			r5.setAmtMilk("4");
 			r5.setAmtSugar("1");
 			r5.setPrice("40");
-			cm.addRecipe(r5); // We try to add a normal recepi
+			cm.addRecipe(r5); // We try to add a normal recipe
 		} catch (RecipeException e) {
-			fail("RecipeException should not be throun.");
+			fail("RecipeException should not be thrown.");
 		}
 		Recipe[] recipes = cm.getRecipes();
 		assertEquals(4, recipes.length); // We check if the number of recipes is correct
-		assertEquals(r5, recipes[4]); // We check if the recipe is correct
+		assertEquals(r5, recipes[0]); // We check if the recipe is correct
 	}
 
 	//6
 	@Test
-	public void testAddRecepiException() {
+	public void testAddRecipeException() {
 		Recipe r5 = new Recipe();
 		Throwable exception = assertThrows(
 				RecipeException.class, () -> {
@@ -145,10 +148,10 @@ public class MainTest {
 
 	//7
 	@Test
-	public void testEditRecepi() {
+	public void testEditRecipe() {
 		Recipe[] recipes = cm.getRecipes();
 		Recipe newRecipe = new Recipe();
-		String recipeEdited;
+		String recipeEdited = null;
 		try {
 			newRecipe.setName("Milk");
 			newRecipe.setPrice("20");
@@ -160,12 +163,12 @@ public class MainTest {
 		} catch (RecipeException e) {
 			fail("RecipeException should not be throun.");
 		}
-		assertEquals(recipeEdited, newRecipe); // Check if they are the same
+		assertEquals(recipeEdited, recipes[0]); // Check if they are the same
 	}
 
 	//8
 	@Test
-	public void testEditRecepiException() {
+	public void testEditRecipeException() {
 		Recipe[] recipes = cm.getRecipes();
 		Recipe newRecipe = new Recipe();
 		Throwable exception = assertThrows(
@@ -180,20 +183,15 @@ public class MainTest {
 				}); // Should throw an Exception
 	}
 
-	//9
+	// 9
 	@Test
-	public void testDeleteARecepi() {
-		try {
-			Recipe[] recipes = cm.getRecipes();
-			for (int i = 0; i < recipes.length; i++) {
-				if (recipes[i] != null) {
-					cm.deleteRecipe(i); // Delete all the recepis
-				}
+	public void testDeleteARecipe() {
+		Recipe[] recipes = cm.getRecipes();
+		for (int i = 0; i < recipes.length; i++) {
+			if (recipes[i] != null) {
+				cm.deleteRecipe(i); // Delete all the recipes
 			}
-		} catch (RecipeException e) {
-			fail("RecipeException should not be throun.");
+			assertEquals(null, recipes[i]); // Check if there are any recipes left
 		}
-		assertTrue(cm.getRecipes() == null); // Check if there are any recepies left
 	}
-
 }
